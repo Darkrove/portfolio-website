f89bfebb2d77f740cac1b4bbb6e82a10301d646a
sajjad <samaralishaikh212@gmail.com>
2023-09-23 19:28:02 +0530

    update color pallette

--- src/app/(new)/new/page.tsx
+++ src/app/(new)/new/page.tsx
@@ -11,18 +11,18 @@ const page = () => {
   const Card04 = "/card-04.png";
   return (
     <body className="relative font-inter antialiased">
-      <main className="relative min-h-screen flex flex-col bg-slate-900 overflow-hidden">
+      <main className="relative min-h-screen flex flex-col bg-background overflow-hidden">
         <div className="w-full max-w-6xl mx-auto px-4 md:px-6 py-24">
           <Spotlight className="max-w-sm mx-auto grid gap-6 lg:grid-cols-3 items-start lg:max-w-none group">
             {/* Card #4 */}
             <SpotlightCard>
-              <div className="relative h-full bg-slate-900 p-6 pb-8 rounded-[inherit] z-20 overflow-hidden">
+              <div className="relative h-full bg-background p-6 pb-8 rounded-[inherit] z-20 overflow-hidden">
                 {/* Radial gradient */}
                 <div
                   className="absolute bottom-0 translate-y-1/2 left-1/2 -translate-x-1/2 pointer-events-none -z-10 w-1/2 aspect-square"
                   aria-hidden="true"
                 >
-                  <div className="absolute inset-0 translate-z-0 bg-slate-800 rounded-full blur-[80px]"></div>
+                  <div className="absolute inset-0 translate-z-0 bg-slate-900 rounded-full blur-[80px]"></div>
                 </div>
                 <div className="flex flex-col h-full justify-center items-center text-center">
                   {/* Image */}
@@ -41,20 +41,20 @@ const page = () => {
                   </div>
                   {/* Text */}
                   {/* <div className="grow mb-5">
-                    <h2 className="text-xl text-slate-200 font-bold mb-1">
+                    <h2 className="text-xl text-foreground font-bold mb-1">
                       Amazing Integration
                     </h2>
-                    <p className="text-sm text-slate-500">
+                    <p className="text-sm text-slate-600">
                       Quickly apply filters to refine your issues lists and
                       create custom views.
                     </p>
                   </div>
                   <a
-                    className="inline-flex justify-center items-center whitespace-nowrap rounded-lg bg-slate-800 hover:bg-slate-900 border border-slate-700 px-3 py-1.5 text-sm font-medium text-slate-300 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300 dark:focus-visible:ring-slate-600 transition-colors duration-150"
+                    className="inline-flex justify-center items-center whitespace-nowrap rounded-lg bg-slate-900 hover:bg-background border border-slate-800 px-3 py-1.5 text-sm font-medium text-slate-400 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300 dark:focus-visible:ring-slate-700 transition-colors duration-150"
                     href="#0"
                   >
                     <svg
-                      className="fill-slate-500 mr-2"
+                      className="fill-slate-600 mr-2"
                       xmlns="http://www.w3.org/2000/svg"
                       width="16"
                       height="14"
@@ -68,13 +68,13 @@ const page = () => {
             </SpotlightCard>
             {/* Card #5 */}
             <SpotlightCard className="lg:col-span-2">
-              <div className="relative h-full bg-slate-900 p-6 pb-8 rounded-[inherit] z-20 overflow-hidden">
+              <div className="relative h-full bg-background p-6 pb-8 rounded-[inherit] z-20 overflow-hidden">
                 {/* Radial gradient */}
                 <div
                   className="absolute bottom-0 translate-y-1/2 left-1/2 -translate-x-1/2 pointer-events-none -z-10 w-1/2 aspect-square"
                   aria-hidden="true"
                 >
-                  <div className="absolute inset-0 translate-z-0 bg-slate-800 rounded-full blur-[80px]"></div>
+                  <div className="absolute inset-0 translate-z-0 bg-slate-900 rounded-full blur-[80px]"></div>
                 </div>
                 <div className="flex flex-col justify-center h-full items-center text-center gap-1">
                   <div className="relative object-contain object-center inline-flex">
@@ -85,31 +85,31 @@ const page = () => {
                   </div>
                   {/* Text */}
                   <div className="flex flex-col items-start justify-start gap-1 w-full h-full">
-                    <h2 className="text-slate-200 text-4xl m-0 font-bold">
+                    <h2 className="text-foreground text-4xl m-0 font-bold">
                       Hi, I&apos;m{" "}
-                      <span className="text-slate-500">Sajjad</span>.
+                      <span className="text-slate-600">Sajjad</span>.
                     </h2>
-                    <h2 className="text-slate-500 text-md m-0 font-bold">
+                    <h2 className="text-slate-600 text-md m-0 font-bold">
                       web developer
                     </h2>
-                    <h2 className="text-slate-200 text-2xl m-0 font-semibold mt-4 text-left">
+                    <h2 className="text-foreground text-2xl m-0 font-semibold mt-4 text-left">
                       Passionate to pursue the technology.
                     </h2>
-                    <h2 className="text-slate-500 text-lg m-0 mt-4 text-left">
+                    <h2 className="text-slate-600 text-lg m-0 mt-4 text-left">
                       I enjoy learning new things and try to overcome new
                       challenges while analyzing how I improved through them.
                     </h2>
                   </div>
 
                   {/* <div className="grow mb-5">
-                    <h2 className="text-slate-200 text-4xl m-0 font-extrabold">
+                    <h2 className="text-foreground text-4xl m-0 font-extrabold">
                       Hi, I&apos;m{" "}
                       <span className="text-slate-400">Sajjad</span>
                     </h2>
-                    <h2 className="text-xl text-slate-200 font-bold mb-1">
+                    <h2 className="text-xl text-foreground font-bold mb-1">
                       Amazing Integration
                     </h2>
-                    <p className="text-sm text-slate-500">
+                    <p className="text-sm text-slate-600">
                       Quickly apply filters to refine your issues lists and
                       create custom views.
                     </p>
@@ -119,13 +119,13 @@ const page = () => {
             </SpotlightCard>
             {/* Card #1 */}
             <SpotlightCard>
-              <div className="relative h-full bg-slate-900 p-6 pb-8 rounded-[inherit] z-20 overflow-hidden">
+              <div className="relative h-full bg-background p-6 pb-8 rounded-[inherit] z-20 overflow-hidden">
                 {/* Radial gradient */}
                 <div
                   className="absolute bottom-0 translate-y-1/2 left-1/2 -translate-x-1/2 pointer-events-none -z-10 w-1/2 aspect-square"
                   aria-hidden="true"
                 >
-                  <div className="absolute inset-0 translate-z-0 bg-slate-800 rounded-full blur-[80px]"></div>
+                  <div className="absolute inset-0 translate-z-0 bg-slate-900 rounded-full blur-[80px]"></div>
                 </div>
                 <div className="flex flex-col h-full items-center text-center">
                   {/* Image */}
@@ -144,20 +144,20 @@ const page = () => {
                   </div>
                   {/* Text */}
                   <div className="grow mb-5">
-                    <h2 className="text-xl text-slate-200 font-bold mb-1">
+                    <h2 className="text-xl text-foreground font-bold mb-1">
                       Amazing Integration
                     </h2>
-                    <p className="text-sm text-slate-500">
+                    <p className="text-sm text-slate-600">
                       Quickly apply filters to refine your issues lists and
                       create custom views.
                     </p>
                   </div>
                   <a
-                    className="inline-flex justify-center items-center whitespace-nowrap rounded-lg bg-slate-800 hover:bg-slate-900 border border-slate-700 px-3 py-1.5 text-sm font-medium text-slate-300 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300 dark:focus-visible:ring-slate-600 transition-colors duration-150"
+                    className="inline-flex justify-center items-center whitespace-nowrap rounded-lg bg-slate-900 hover:bg-background border border-slate-800 px-3 py-1.5 text-sm font-medium text-slate-400 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300 dark:focus-visible:ring-slate-700 transition-colors duration-150"
                     href="#0"
                   >
                     <svg
-                      className="fill-slate-500 mr-2"
+                      className="fill-slate-600 mr-2"
                       xmlns="http://www.w3.org/2000/svg"
                       width="16"
                       height="14"
@@ -171,13 +171,13 @@ const page = () => {
             </SpotlightCard>
             {/* Card #2 */}
             <SpotlightCard>
-              <div className="relative h-full bg-slate-900 p-6 pb-8 rounded-[inherit] z-20 overflow-hidden">
+              <div className="relative h-full bg-background p-6 pb-8 rounded-[inherit] z-20 overflow-hidden">
                 {/* Radial gradient */}
                 <div
                   className="absolute bottom-0 translate-y-1/2 left-1/2 -translate-x-1/2 pointer-events-none -z-10 w-1/2 aspect-square"
                   aria-hidden="true"
                 >
-                  <div className="absolute inset-0 translate-z-0 bg-slate-800 rounded-full blur-[80px]"></div>
+                  <div className="absolute inset-0 translate-z-0 bg-slate-900 rounded-full blur-[80px]"></div>
                 </div>
                 <div className="flex flex-col h-full items-center text-center">
                   {/* Image */}
@@ -196,20 +196,20 @@ const page = () => {
                   </div>
                   {/* Text */}
                   <div className="grow mb-5">
-                    <h2 className="text-xl text-slate-200 font-bold mb-1">
+                    <h2 className="text-xl text-foreground font-bold mb-1">
                       Amazing Integration
                     </h2>
-                    <p className="text-sm text-slate-500">
+                    <p className="text-sm text-slate-600">
                       Quickly apply filters to refine your issues lists and
                       create custom views.
                     </p>
                   </div>
                   <a
-                    className="inline-flex justify-center items-center whitespace-nowrap rounded-lg bg-slate-800 hover:bg-slate-900 border border-slate-700 px-3 py-1.5 text-sm font-medium text-slate-300 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300 dark:focus-visible:ring-slate-600 transition-colors duration-150"
+                    className="inline-flex justify-center items-center whitespace-nowrap rounded-lg bg-slate-900 hover:bg-background border border-slate-800 px-3 py-1.5 text-sm font-medium text-slate-400 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300 dark:focus-visible:ring-slate-700 transition-colors duration-150"
                     href="#0"
                   >
                     <svg
-                      className="fill-slate-500 mr-2"
+                      className="fill-slate-600 mr-2"
                       xmlns="http://www.w3.org/2000/svg"
                       width="16"
                       height="14"
@@ -223,13 +223,13 @@ const page = () => {
             </SpotlightCard>
             {/* Card #3 */}
             <SpotlightCard>
-              <div className="relative h-full bg-slate-900 p-6 pb-8 rounded-[inherit] z-20 overflow-hidden">
+              <div className="relative h-full bg-background p-6 pb-8 rounded-[inherit] z-20 overflow-hidden">
                 {/* Radial gradient */}
                 <div
                   className="absolute bottom-0 translate-y-1/2 left-1/2 -translate-x-1/2 pointer-events-none -z-10 w-1/2 aspect-square"
                   aria-hidden="true"
                 >
-                  <div className="absolute inset-0 translate-z-0 bg-slate-800 rounded-full blur-[80px]"></div>
+                  <div className="absolute inset-0 translate-z-0 bg-slate-900 rounded-full blur-[80px]"></div>
                 </div>
                 <div className="flex flex-col h-full items-center text-center">
                   {/* Image */}
@@ -248,20 +248,20 @@ const page = () => {
                   </div>
                   {/* Text */}
                   <div className="grow mb-5">
-                    <h2 className="text-xl text-slate-200 font-bold mb-1">
+                    <h2 className="text-xl text-foreground font-bold mb-1">
                       Amazing Integration
                     </h2>
-                    <p className="text-sm text-slate-500">
+                    <p className="text-sm text-slate-600">
                       Quickly apply filters to refine your issues lists and
                       create custom views.
                     </p>
                   </div>
                   <a
-                    className="inline-flex justify-center items-center whitespace-nowrap rounded-lg bg-slate-800 hover:bg-slate-900 border border-slate-700 px-3 py-1.5 text-sm font-medium text-slate-300 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300 dark:focus-visible:ring-slate-600 transition-colors duration-150"
+                    className="inline-flex justify-center items-center whitespace-nowrap rounded-lg bg-slate-900 hover:bg-background border border-slate-800 px-3 py-1.5 text-sm font-medium text-slate-400 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300 dark:focus-visible:ring-slate-700 transition-colors duration-150"
                     href="#0"
                   >
                     <svg
-                      className="fill-slate-500 mr-2"
+                      className="fill-slate-600 mr-2"
                       xmlns="http://www.w3.org/2000/svg"
                       width="16"
                       height="14"
