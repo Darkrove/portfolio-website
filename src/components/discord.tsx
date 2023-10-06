494f37ada28bd71e334674954408b33cefe46005
sajjad <samaralishaikh212@gmail.com>
2023-10-06 17:22:04 +0530

    added zod pkg

--- src/components/discord.tsx
+++ src/components/discord.tsx
@@ -1,5 +1,6 @@
 import type { LanyardResponse } from "@/lib/types";
 import { SiDiscord, SiLeetcode } from "react-icons/si";
+import { AiFillStar } from "react-icons/ai";
 import clsx from "clsx";
 import { apiUrls } from "@/lib/api-urls";
 import { Separator } from "@/ui/separator";
@@ -26,9 +27,21 @@ const fetchLeetCode = async () => {
   return await res.json();
 };
 
+const fetchGithub = async () => {
+  const res = await fetch(apiUrls.github.get("darkrove"), {
+    next: { revalidate: 10 },
+  });
+  if (!res.ok) {
+    const error = await res.json();
+    throw error;
+  }
+  return await res.json();
+};
+
 export async function Discord() {
   const response = await getData();
   const res = await fetchLeetCode();
+  const githubRes = await fetchGithub();
   const data: LanyardResponse = response.data;
   const rank = res.data.userProfile.profile.ranking;
   return (
@@ -52,8 +65,12 @@ export async function Discord() {
         {data?.discord_status || "offline"}
       </p>
       <Separator orientation="vertical" />
-      <p className="dark:text-orange-400 text-orange-600 m-0 text-xs lg:text-sm md:text-sm flex justify-center items-center">
+      <p className="dark:text-purple-400 text-orange-600 m-0 text-xs lg:text-sm md:text-sm flex justify-center items-center">
         <SiLeetcode className="inline-block mr-1 w-4 h-4" /> {rank}
+      </p>
+      <Separator orientation="vertical" />
+      <p className="dark:text-yellow-400 text-orange-600 m-0 text-xs lg:text-sm md:text-sm flex justify-center items-center">
+        <AiFillStar className="inline-block mr-1 w-4 h-4" /> {githubRes.stars}
       </p>
     </div>
   );
