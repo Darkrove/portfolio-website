93a45e8833b06c8aa50a9e8361f38059a25fc471
sajjad <samaralishaikh212@gmail.com>
2023-10-07 12:26:30 +0530

    update umami route

--- src/app/api/umami/route.ts
+++ src/app/api/umami/route.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
 
 const dynamic = "force-dynamic";
 
@@ -48,10 +49,45 @@ async function getAnalytics() {
 }
 
 export async function GET(request: NextRequest) {
-  const resp = await getAnalytics();
-  const analytics = await resp.json();
+  try {
+    const response = await fetch(
+      "https://umami-darkspace.vercel.app/api/auth/login",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: process.env.UMAMI_USERNAME,
+          password: process.env.UMAMI_PASSWORD,
+        }),
+      }
+    );
 
-  return new Response(JSON.stringify(analytics), {
-    status: 200,
-  });
+    const accessToken = await response.json();
+    const resp = await fetch(
+      `https://umami-darkspace.vercel.app/api/websites/6c2507b8-6434-42d4-8775-f0fea81d3b90/stats?start_at=1666463400000&end_at=${Date.now()}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${accessToken?.token}`,
+        },
+      }
+    );
+
+    const analytics = await resp.json();
+
+    return new Response(JSON.stringify(analytics), {
+      status: 200,
+    });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      console.log(error);
+      return NextResponse.json({ error: error.issues }, { status: 400 });
+    }
+    return NextResponse.json(
+      { error, message: "Failed to get the data" },
+      { status: 500 }
+    );
+  }
 }
