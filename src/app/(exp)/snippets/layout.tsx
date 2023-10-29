78cc37b227eda30214152235af29dc99971a913e
sajjad <samaralishaikh212@gmail.com>
2023-10-29 20:56:53 +0530

    added layout

---
+++ src/app/(exp)/snippets/layout.tsx
@@ -0,0 +1,18 @@
+import { Metadata } from "next";
+import NavBar from "@/components/nav";
+import { siteConfig } from "@/config/site";
+
+export const metadata: Metadata = {
+  title: {
+    default: "Snippets",
+    template: `%s | ${siteConfig.name}`,
+  },
+};
+
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return <>{children}</>;
+}
