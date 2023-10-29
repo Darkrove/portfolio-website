015902a3f09a43fee6c1b57b0dbbf60fbf75e363
sajjad <samaralishaikh212@gmail.com>
2023-10-29 20:47:12 +0530

    added snippet page

---
+++ src/app/(exp)/snippets/page.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import Label from "@/components/label";
+import Layout from "@/components/layout";
+import Bar from "@/components/bar";
+import Rhythm from "./rhythm";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+import { ArrowUpRight } from "lucide-react";
+
+interface Props {}
+
+const page = () => {
+  return (
+    <>
+      <Layout>
+        <div className="w-full flex flex-col space-y-4 justify-center items-center py-4 sm:py-8">
+          <Bar />
+          <div className="flex flex-col gap-3 w-full justify-start px-8">
+            <Label size="sm">Cool Animation</Label>
+            <Card>
+              <CardHeader>
+                <CardTitle className="text-base">Spring Animation</CardTitle>
+                <CardDescription className="text-sm">
+                  Auto scale animation using useSpring.
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <div className="w-full relative h-[300px] c overflow-hidden flex flex-col justify-center items-center">
+                  <Rhythm className="absolute inset-0 z-0" />
+                  <div className="relative z-10 p-8 min-h-full flex justify-center items-center">
+                    <Label size="lg">Cool Animation</Label>
+                  </div>
+                </div>
+              </CardContent>
+              <CardFooter className="text-sm">
+                <Link
+                  href="https://github.com/Darkrove/portfolio-website/tree/main/src/app/(exp)/snippets/rhythm.tsx"
+                  target="_blank"
+                  className={buttonVariants({ variant: "outline", size: "lg" })}
+                >
+                  View Code
+                  <ArrowUpRight className="w-4 h-4 ml-2" />
+                </Link>
+              </CardFooter>
+            </Card>
+          </div>
+        </div>
+      </Layout>
+    </>
+  );
+};
+
+export default page;
---
+++ src/app/(exp)/snippets/rhythm.tsx
@@ -0,0 +1,60 @@
+"use client";
+import { Circle } from "lucide-react";
+import React, { useEffect, useState, useRef } from "react";
+import { motion, useSpring } from "framer-motion";
+import { cn } from "@/lib/utils";
+
+const BIG_SIZE = 80;
+const SMALL_SIZE = 15;
+const PER_PX = 0.3;
+
+interface Props extends React.HTMLAttributes<HTMLDivElement> {}
+
+export default function Rhythm({ className }: Props) {
+  const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
+  useEffect(() => {
+    const handler = (e: MouseEvent) => {
+      setMousePos({ x: e.clientX, y: e.clientY });
+    };
+    window.addEventListener("mousemove", handler);
+    return () => {
+      window.removeEventListener("mousemove", handler);
+    };
+  });
+  return (
+    <div
+      className={cn(
+        "flex flex-wrap gap-20 mx-auto p-12 justify-between",
+        className
+      )}
+    >
+      {Array.from({ length: 100 }, (_, i) => (
+        <Dot key={i} mousePos={mousePos} />
+      ))}
+    </div>
+  );
+}
+
+function Dot({ mousePos }: { mousePos: { x: number; y: number } }) {
+  const size = useSpring(SMALL_SIZE, { damping: 30, stiffness: 200 });
+  const dotRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (!dotRef.current) return;
+    const { x, y } = mousePos;
+    const { x: dotX, y: dotY } = dotRef.current.getBoundingClientRect();
+
+    const distance = Math.sqrt(
+      Math.pow(Math.abs(x - dotX), 2) + Math.pow(Math.abs(y - dotY), 2)
+    );
+    size.set(Math.max(BIG_SIZE - PER_PX * distance, SMALL_SIZE));
+  }, [mousePos, size]);
+
+  return (
+    <div ref={dotRef} className="relative">
+      <motion.div
+        className="bg-green-400 rounded-full absolute -translate-y-1/2 -translate-x-1/2"
+        style={{ width: size, height: size }}
+      />
+    </div>
+  );
+}
