911a58457ca3ef4bdc3ff38c175fcfc082e4d713
sajjad <samaralishaikh212@gmail.com>
2024-01-14 18:23:18 +0530

    removed link from CTA

--- src/components/cta.tsx
+++ src/components/cta.tsx
@@ -21,9 +21,7 @@ const Cta = () => {
           Chat With Me
           <Icons.whatsapp className="ml-2 w-5 h-5" />
         </Link>
-        <Link
-          href="https://www.linkedin.com/feed/update/urn:li:activity:7124769450238631936/"
-          target="_blank"
+        <div
           className={cn(
             buttonVariants({ size: "lg", variant: "outline" }),
             "dark:text-emerald-400 text-emerald-600 hover:text-emerald-600"
@@ -32,7 +30,7 @@ const Cta = () => {
           Data Specialist
           <span className="font-bold uppercase">, HERE Technologies!</span>
           <Icons.pin className="ml-2 w-5 h-5" />
-        </Link>
+        </div>
       </div>
     </div>
   );
